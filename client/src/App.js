e23497a3454813f856eefadfb3215547f0dfd6cf
Evgen941-max <Kolomijchyk@hotmail.com>
2022-01-06 11:15:41 +0200

    modified client

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,9 @@ import axios from 'axios'
 import {List} from './List'
 import {Form} from './Form'
 
-
-const baseURL = 'http://localhost:5000'
+const baseURL = process.env.NODE_ENV === 'production' 
+  ? 'http://localhost:5000'
+  : 'http://localhost:5000'
 
 const api = axios.create({ baseURL })
 
